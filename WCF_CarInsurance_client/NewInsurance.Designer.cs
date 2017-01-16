cb6212ba7b0052b4573a3becd7f9a06bde3fc7b6
Michał <michal.dwojak92@gmail.com>
2017-01-16 18:23:04 +0100

    Edit - text in label in form new insurance

--- WCF_CarInsurance_client/NewInsurance.Designer.cs
+++ WCF_CarInsurance_client/NewInsurance.Designer.cs
@@ -262,9 +262,9 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.Location = new System.Drawing.Point(6, 16);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.Size = new System.Drawing.Size(70, 16);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Begin Date:";
+            this.label2.Text = "Start Date:";
             // 
             // NewInsurance
             // 
