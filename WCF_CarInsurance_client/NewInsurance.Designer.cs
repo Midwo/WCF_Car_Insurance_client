79feffc3093be599e31365c7a0121910e775cae9
Michał <michal.dwojak92@gmail.com>
2017-01-17 23:56:26 +0100

    Edit- text in label

--- WCF_CarInsurance_client/NewInsurance.Designer.cs
+++ WCF_CarInsurance_client/NewInsurance.Designer.cs
@@ -204,17 +204,17 @@
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(137, 16);
             this.label4.TabIndex = 10;
-            this.label4.Text = "Description Package:";
+            this.label4.Text = "Package Description:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(384, 14);
+            this.label3.Location = new System.Drawing.Point(384, 13);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(103, 16);
             this.label3.TabIndex = 8;
-            this.label3.Text = "Description Car:";
+            this.label3.Text = "Car Description:";
             // 
             // textBox1
             // 
